c6405fb8e58bca2fbba10034a9b08b444dfb40ff
zhaochen <306232627@qq.com>
2025-07-08 12:01:13 +0800

    opengauss分区表迁移

--- migration-tool/build.sh
+++ migration-tool/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mvn clean package -Dmaven.test.skip=true
 mkdir full-migration-tool
-cp target/full-magration-jar-with-dependencies.jar full-migration-tool/full-migration-tool-7.0.0-RC2.jar
+cp target/full-magration-jar-with-dependencies.jar full-migration-tool/full-migration-tool-7.0.0rc2.jar
 cp -r config full-migration-tool/
-tar -czf full-migration-tool-7.0.0-RC2.tar.gz full-migration-tool
+tar -czf full-migration-tool-7.0.0rc2.tar.gz full-migration-tool
 rm -rf full-migration-tool
-mv full-migration-tool-7.0.0-RC2.tar.gz target/
+mv full-migration-tool-7.0.0rc2.tar.gz target/
